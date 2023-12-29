60ef8a46662cbd7b06806ee7998a80978cf21a1c
welingtonsoftware <dpepsloco@gmail.com>
2023-12-29 15:36:34 -0300

    feat alter class cityControl prisma dependece

--- src/controllers/cityController.ts
+++ src/controllers/cityController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
-import { PrismaClient } from "@prisma/client";
+import { prismaClient } from "../lib/prisma";
 
-const prisma = new PrismaClient();
+const prisma = prismaClient;
 
 const { findCityId, chekCityExists, getCityById } = require('../services/cityServices');
 
